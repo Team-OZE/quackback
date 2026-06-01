2a53e0322c7ec7a7ac4ace54b92294e0cc06b343
wolke <davidskan3@gmail.com>
2026-06-01 16:06:17 +0200

    warn instead of err on unused vars

--- eslint.config.js
+++ eslint.config.js
@@ -33,7 +33,7 @@ export default tseslint.config(
     ignores: dbReexportFiles,
     rules: {
       "@typescript-eslint/no-unused-vars": [
-        "error",
+        "warn",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
       ],
       "@typescript-eslint/no-explicit-any": "warn",
